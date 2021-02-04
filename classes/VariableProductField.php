d79e6699b5f1de2ec57ade71429b01b27396282d
Ashley Redman <ash.redman@outlook.com>
2021-02-04 19:28:38 +0000

    Set up the methods to create, save & load the custom fields for a variation that meets the term criteria.

--- classes/VariableProductField.php
+++ classes/VariableProductField.php
@@ -2,8 +2,105 @@
 
 namespace clothes2order\classes;
 
-class VariableProductField {
+class VariableProductField
+{
+    /**
+     * @param $variation
+     *
+     * @return bool
+     */
+    public function checkIfHasTerm($variation): bool
+    {
+        $product_variation = wc_get_product($variation);
+        $product = wc_get_product($product_variation->get_parent_id());
 
-    // TODO create a way for a field to be entered & created
+        $term = get_term_by('slug', 'clothes2order-clothing', 'product_cat');
+
+        // check if variation parent product has the term, not the variation.
+        if (has_term($term->term_id, 'product_cat', $product->ID)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * TODO Set the custom fields we need for this variation of a product
+     *
+     * @param $loop
+     * @param $variation_data
+     * @param $variation
+     */
+    public function variation_settings_fields($loop, $variation_data, $variation)
+    {
+        var_dump('SET settings');
+        die();
+//        woocommerce_wp_checkbox(
+//            array(
+//                'id' => "has_class_dates{$loop}",
+//                'name' => "has_class_dates[{$loop}]",
+//                'value' => get_post_meta($variation->ID, 'has_class_dates', true),
+//                'label' => __('&nbsp; Has Class Dates?', 'woocommerce'),
+//                'desc_tip' => true,
+//                'description' => __('Select if this variation is for a class/course.', 'woocommerce'),
+//                'wrapper_class' => 'form-row form-row-full',
+//                'required' => true
+//            )
+//        );
+//
+//        woocommerce_wp_text_input(
+//            array(
+//                'id' => "class_start_date{$loop}",
+//                'name' => "class_start_date[{$loop}]",
+//                'value' => get_post_meta($variation->ID, 'class_start_date', true),
+//                'label' => __('Class Start Date', 'woocommerce'),
+//                'desc_tip' => true,
+//                'description' => __('The start date of this variation.', 'woocommerce'),
+//                'wrapper_class' => 'form-row form-row-first',
+//            )
+//        );
+    }
+
+    /**
+     * Save variation inputs on post update
+     *
+     * @param $variation_id
+     * @param $loop
+     */
+    public function save_variation_settings_fields($variation_id, $loop)
+    {
+        var_dump('SAVE settings');
+        die();
+
+//        $has_dates = $_POST['has_class_dates'][$loop]; // yes(weird) or null
+//
+//        if (!is_null($has_dates)) {
+//            $start_date = $_POST['class_start_date'][$loop]; // string or empty string
+//            $end_date = $_POST['class_end_date'][$loop]; // string or empty string
+//
+//            if (!empty($start_date) && !empty($end_date)) {
+//                update_post_meta($variation_id, 'has_class_dates', esc_attr($has_dates));
+//                update_post_meta($variation_id, 'class_start_date', esc_attr($start_date));
+//                update_post_meta($variation_id, 'class_end_date', esc_attr($end_date));
+//            }
+//        }
+    }
+
+    /**
+     * Load variation custom fields on post open
+     *
+     * @param $variation
+     */
+    public function load_variation_settings_fields($variation)
+    {
+        var_dump('LOAD settings');
+        die();
+
+//        $variation['has_class_dates'] = get_post_meta($variation['variation_id'], 'has_class_dates', true);
+//        $variation['class_start_date'] = get_post_meta($variation['variation_id'], 'class_start_date', true);
+//        $variation['class_end_date'] = get_post_meta($variation['variation_id'], 'class_end_date', true);
+//
+//        return $variation;
+    }
 
 }
--- clothes2order.php
+++ clothes2order.php
@@ -39,7 +39,9 @@ add_action('plugins_loaded', function () {
         add_action('init', 'createProductCatTerms');
 
         // 2. Add any additional product fields
-
+        add_action('woocommerce_product_after_variable_attributes', 'productVariationFieldsSettings', 10, 3);
+        add_action('woocommerce_save_product_variation', 'productVariationFieldsSave', 10, 2);
+        add_filter('woocommerce_available_variation', 'productVariationFieldsLoad');
 
         // 3. On payment complete, 'run' the basket & post API calls for each basket item if meeting requirement
         add_action('woocommerce_payment_complete', 'processNewOrder');
@@ -50,14 +52,21 @@ add_action('plugins_loaded', function () {
 });
 
 /**
+ * Docs: https://developer.wordpress.org/reference/hooks/admin_notices/
+ */
+function setAdminNoticeError()
+{
+    echo '<div class="notice notice-error"><p>' . _('Woocommerce is required to use the Clothes2Order Plugin!') . '</p></div>';
+}
+
+/**
  *
  */
 function createProductCatTerms(): void
 {
     require_once plugin_dir_path(__FILE__) . '/classes/ProductTerms.php';
     $productTerms = new clothes2order\classes\ProductTerms();
-    $productTerms->ensureTermsExist('product_cat', 'class', 'Class', 'Class Products');
-    $productTerms->ensureTermsExist('product_cat', 'clothing', 'Clothing', 'Clothing Products');
+    $productTerms->ensureTermsExist('product_cat', 'clothes2order-clothing', 'Clothing', 'Clothing Products');
 }
 
 /**
@@ -70,9 +79,49 @@ function processNewOrder($order_id)
 }
 
 /**
- * Docs: https://developer.wordpress.org/reference/hooks/admin_notices/
+ * @param $loop
+ * @param $variation_data
+ * @param $variation
  */
-function setAdminNoticeError()
+function productVariationFieldsSettings($loop, $variation_data, $variation)
 {
-    echo '<div class="notice notice-error"><p>' . _('Woocommerce is required to use the Clothes2Order Plugin!') . '</p></div>';
+    require_once plugin_dir_path(__FILE__) . '/classes/VariableProductField.php';
+    $fields = new clothes2order\classes\VariableProductField();
+
+    if ($fields->checkIfHasTerm($variation)) {
+        $fields->variation_settings_fields($loop, $variation_data, $variation);
+    }
+}
+
+/**
+ * @param $variation_id
+ * @param $loop
+ */
+function productVariationFieldsSave($variation_id, $loop)
+{
+    var_dump('SAVE');
+    die();
+    require_once plugin_dir_path(__FILE__) . '/classes/VariableProductField.php';
+    $fields = new clothes2order\classes\VariableProductField();
+
+    $variation = wc_get_product($variation_id);
+
+    if ($fields->checkIfHasTerm($variation)) {
+        $fields->save_variation_settings_fields($variation_id, $loop);
+    }
+}
+
+/**
+ * @param $variation
+ */
+function productVariationFieldsLoad($variation)
+{
+    var_dump('LOAD');
+    die();
+    require_once plugin_dir_path(__FILE__) . '/classes/VariableProductField.php';
+    $fields = new clothes2order\classes\VariableProductField();
+
+    if ($fields->checkIfHasTerm($variation)) {
+        $fields->load_variation_settings_fields($variation);
+    }
 }
