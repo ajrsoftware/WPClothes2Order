09035f0318f05e1d1b0292315d74a6bc3457a912
Ashley Redman <ash.redman@outlook.com>
2022-08-31 12:01:19 +0100

    Updating admin options page

---
+++ classes/Options.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace WPClothes2Order;
+
+class Options
+{
+    public function __construct()
+    {
+        add_filter('woocommerce_get_sections_products', [$this, 'wpc2o_options_page']);
+        add_filter('woocommerce_get_settings_products', [$this, 'wpc2o_options_page_settings'], 10, 2);
+    }
+
+    public function wpc2o_options_page($sections)
+    {
+        $sections['wpc2o'] = __('WPClothes2Order', 'wpc2o');
+        return $sections;
+    }
+
+    public function wpc2o_options_page_settings($settings, $current_section)
+    {
+        if ($current_section === 'wpc2o') {
+            $wpc2o_settings = [];
+
+            $wpc2o_settings[] = [
+                'name' => __('WPClothes2Order Settings', 'wpc2o'),
+                'type' => 'title',
+                'desc' => __('The following options are used to configure the Clothes2Order connection & WooCommerce settings.', 'wpc2o'),
+                'id' => 'wpc2o'
+            ];
+
+            $wpc2o_settings[] = [
+                'name' => __('API Key', 'wpc2o'),
+                'desc_tip' => __('This is the unique API key provided by Clothes2Order', 'wpc2o'),
+                'id' => 'wpc2o_api_key',
+                'type' => 'password',
+                'desc' => __('API key provided by Clothes2Order', 'wpc2o'),
+            ];
+
+            $wpc2o_settings[] = [
+                'name' => __('API Endpoint', 'wpc2o'),
+                'desc_tip' => __('This is the unique URI that is used to communicate with Clothes2Order', 'wpc2o'),
+                'id' => 'wpc2o_endpoint',
+                'type' => 'text',
+                'desc' => __('Endpoint URL provided by Clothes2Order', 'wpc2o'),
+            ];
+
+            $wpc2o_settings[] = [
+                'name' => __('Store manager email', 'wpc2o'),
+                'desc_tip' => __('Please enter an email address', 'wpc2o'),
+                'id' => 'wpc2o_manager_email',
+                'type' => 'email',
+                'desc' => __('This address will receive failed order email notifications', 'wpc2o'),
+            ];
+
+            $wpc2o_settings[] = [
+                'name' => __('Test Mode', 'wpc2o'),
+                'desc_tip' => __('In test mode, orders will not be sent to Clothes2Order', 'wpc2o'),
+                'id' => 'wpc2o_test_mode',
+                'type' => 'checkbox',
+                'desc' => __('Check to enable test mode', 'wpc2o'),
+            ];
+
+            $settings_c2o[] = [
+                'type' => 'sectionend', 'id' => 'wpc2o'
+            ];
+
+            return $wpc2o_settings;
+        }
+
+        return $settings;
+    }
+}
--- wpclothes2order.php
+++ wpclothes2order.php
@@ -1,23 +1,23 @@
 <?php
 
 /**
- * WPClothes2Order
- * @package   wpclothes2order
- * @link      https://www.wpclothes2order.com
- * @author    Reuben Porter <porterdmu@gmail.com> & Ashley Redman <ash.redman@outlook.com>
- * @copyright 2021 AR Development
- * @license   GPL v2 or later
- *
  * Plugin Name:  WPClothes2Order
- * Description:  Clothes2Order WordPress Plugin
- * Version:      1.0
+ * Description:  Unofficial WooCommerce Plugin for <a href="https://www.clothes2order.com/">Clothes2Order</a>
+ * Version:      1.0.0
  * Plugin URI:   https://www.wpclothes2order.com
- * Author:       Reuben Porter & Ashley Redman
- * Author URI:   
- * Text Domain:  wp-clothes-2-order
+ * Author:       Ashley Redman
+ * Author URI:   https://github.com/AshleyRedman
+ * Text Domain:  wpc2o
  * Domain Path:
+ * Requires at least: 6.0.0
  * Requires PHP: 7.4
  *
+ * @package   wpclothes2order
+ * @link      https://www.wpclothes2order.com
+ * @author    Ashley Redman <ash.redman@outlook.com>
+ * @copyright 2022 Ashley Redman
+ * @license   GPL v2 or later
+ * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -29,6 +29,19 @@
  * GNU General Public License for more details.
  */
 
-defined('ABSPATH') || die();
+namespace WPClothes2Order;
 
-// todo
+defined('ABSPATH') || exit;
+
+add_action('plugins_loaded', function () {
+    if (class_exists('Woocommerce')) {
+        if (!class_exists('WPClothes2Order\Options')) {
+            require_once('classes/Options.php');
+            new Options;
+        }
+    } else {
+        add_action('admin_notices', function () {
+            echo '<div class="notice notice-error"><p>' . _('Woocommerce is required to use WPClothes2Order!', 'wp-clothes-2-order') . '</p></div>';
+        }, 10, 2);
+    }
+});
