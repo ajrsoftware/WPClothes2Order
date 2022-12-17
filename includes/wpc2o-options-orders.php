cda1626c026f3570246718bd84df6c4fbba6019c
Ashley Redman <ash.redman@outlook.com>
2022-12-17 12:20:14 +0000

    Update wpc2o-options-orders.php

--- includes/wpc2o-options-orders.php
+++ includes/wpc2o-options-orders.php
@@ -88,14 +88,20 @@ function wpc2o_view_order_payload($order)
     $record   = get_post_meta($order->ID, '_wpc2o_order_api_payload', true);
     $response = get_post_meta($order->ID, '_wpc2o_order_api_response', true);
 
+    $endpoint = is_array($record) ? $record['endpoint'] : '';
+    $headers = is_array($record) ? $record['headers'] : '';
+    $code = is_array($response) ? $response['code'] : '';
+    $message = is_array($response) ? $response['message'] : '';
+    $body = is_array($response) ? htmlspecialchars($response['body']) : '';
+
     $content  = '<div class="wpc2o-view-payload-modal-inner">';
     $content .= '<button class="wpc2o-view-payload-modal-copy button">Copy to clipboard</button>';
     $content .= '<button class="wpc2o-view-payload-modal-close button button-primary">Close</button>';
-    $content .= '<div>Endpoint: ' . $record['endpoint'] . '</div>';
-    $content .= '<div>Headers: ' . $record['headers'] . '<div>';
-    $content .= '<div>Response code: ' . $response['code'] . '<div>';
-    $content .= '<div>Response message: ' . $response['message'] . '<div>';
-    $content .= '<div>Response body: ' . htmlspecialchars($response['body']) . '<div>';
+    $content .= '<div>Endpoint: ' . $endpoint . '</div>';
+    $content .= '<div>Headers: ' . $headers . '<div>';
+    $content .= '<div>Response code: ' . $code . '<div>';
+    $content .= '<div>Response message: ' . $message . '<div>';
+    $content .= '<div>Response body: ' . $body . '<div>';
     $content .= '<div>Payload sent:<div>';
     $content .= '<pre class="wpc2o-view-payload-modal-content"><code>';
     $content .= $record['body'];
