28adb616a996b93ba802e4a7fb1ce70eec50aea2
Ashley Redman <ashley.redman@ajrsoftware.com>
2024-10-02 19:32:37 +0100

    bump

--- SECURITY.md
+++ SECURITY.md
@@ -4,8 +4,8 @@
 
 | Version | Supported          |
 | ------- | ------------------ |
-| 1.1.5   | :white_check_mark: |
-| > 1.1.5 | :x:                |
+| 1.1.6   | :white_check_mark: |
+| > 1.1.6 | :x:                |
 
 ## Reporting a Vulnerability
 
--- src/ts/stock-sync.ts
+++ src/ts/stock-sync.ts
@@ -7,17 +7,21 @@ export const sync = async (): Promise<Response | null> => {
     let res: Response | null = null;
 
     try {
-        const response = await fetch(`/wp-json/wpc2o/v1/stock-sync`, {
+        const response = await fetch(`/wp-json/wpc2o/v1/stock-sync/`, {
             method: 'get',
             headers: {
                 'Content-Type': 'application/json'
             }
         });
+        res = await response.json();
+
+        console.log(res);
 
         if (response.ok) {
             res = await response.json();
         }
     } catch (err) {
+        console.log({ err });
         res = {
             status: false,
             message: 'There was a problem requesting a stock sync.'
